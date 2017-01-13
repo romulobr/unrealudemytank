1d480f63f0ba73088d6e38ecb6dd0f7b532195e4
Romulo Santos <romulobr@gmail.com>
2017-01-13 20:28:33 +0100

    progress on aiming system

--- Source/battletank/TankPlayerController.cpp
+++ Source/battletank/TankPlayerController.cpp
@@ -7,26 +7,65 @@ void ATankPlayerController::BeginPlay()
 {
 	Super::BeginPlay();
 	auto controlledTank = GetControlledTank();
-	
-	if(controlledTank)
+
+	if (controlledTank)
 	{
 		UE_LOG(LogTemp, Warning, TEXT("Player tank: %s"), *controlledTank->GetName());
-	}	
+	}
 }
 
 void ATankPlayerController::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	UE_LOG(LogTemp, Warning, TEXT("Player controller ticked"));
+	aimAtCrosshair();
 }
 
-void ATankPlayerController::aimAtCrosshair()
+void ATankPlayerController::aimAtCrosshair() const
 {
 	auto controlledTank = GetControlledTank();
-	if(!controlledTank)
+	if (controlledTank)
 	{
-		
+		OUT FVector hitLocation;
+		if (gotSightRayHitLocationIn(hitLocation))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Hit location is: %s\n"), *hitLocation.ToString());
+		}
 	}
+}
+
+bool ATankPlayerController::gotAimingCoordinatesInto(FVector& worldLocation, FVector& lookDirection) const
+{
+	int32 OUT viewPortSizeX, OUT viewPortSizeY;
+	GetViewportSize(OUT viewPortSizeX, OUT viewPortSizeY);
+	FVector2D screenLocation = FVector2D(viewPortSizeX * crossHairXLocation, viewPortSizeY * crossHairYLocation);
+	return DeprojectScreenPositionToWorld(screenLocation.X, screenLocation.Y, worldLocation, lookDirection);
+}
+
+bool ATankPlayerController::hitSomethingAndPutResultsInto(OUT FHitResult& result, FVector* worldLocation, FVector* lookDirection) const
+{
+	auto start = *worldLocation;
+	auto end = *worldLocation + (*lookDirection * 100000.0f);
+	DrawDebugLine(GetWorld(), start, end, FColor(255, 255, 100), false, 0.0f, 0.0f, 3.0f);
+	UE_LOG(LogTemp, Warning, TEXT("\n\n\nstart: %s end %s\n\n\n"), *start.ToString(), *end.ToString());
+	return GetWorld()->LineTraceSingleByChannel(result, start, end, ECC_Visibility);
+}
+
+bool ATankPlayerController::gotSightRayHitLocationIn(OUT FVector& hitLocation) const
+{
+	OUT FVector worldLocation;
+	OUT FVector lookDirection;
+
+	if (gotAimingCoordinatesInto(worldLocation, lookDirection))
+	{
+		OUT FHitResult result;
+		if (hitSomethingAndPutResultsInto(result, &worldLocation, &lookDirection))
+		{
+			UE_LOG(LogTemp, Warning, TEXT("Hit: %s\n"), *result.ToString());
+			hitLocation = result.ImpactPoint;
+			return true;
+		};
+	}
+	return false;
 }
 
 ATank* ATankPlayerController::GetControlledTank() const
--- Source/battletank/TankPlayerController.h
+++ Source/battletank/TankPlayerController.h
@@ -9,16 +9,30 @@
  *
  */
 UCLASS()
+
 class BATTLETANK_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
 private:
-	void aimAtCrosshair();
-		
-	public:
-		ATank* GetControlledTank() const;
-	
-		void BeginPlay() override;
-		void Tick(float DeltaTime);
+	const FCollisionQueryParams colisionQueryParams = FCollisionQueryParams(FName(TEXT("")), false, GetOwner());
+	const FCollisionObjectQueryParams objectQueryParams = FCollisionObjectQueryParams(ECollisionChannel::ECC_Visibility);
+
+	void aimAtCrosshair() const;
+	bool gotAimingCoordinatesInto(FVector& worldLocation, FVector& lookDirection) const;
+	bool hitSomethingAndPutResultsInto(FHitResult& result, FVector* worldLocation, FVector* lookDirection) const;
+
+	bool gotSightRayHitLocationIn(OUT FVector& hitLocation) const;
+
+public:
+	ATank* GetControlledTank() const;
+
+	void BeginPlay() override;
+	void Tick(float DeltaTime);
+
+	UPROPERTY(EditAnywhere)
+	float crossHairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float crossHairYLocation = 0.33333;
 };
